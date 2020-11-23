8d65e0eadcf6e1c3727f5988048fd2016289d9d4
shwetakundra <shwetakundra688@gmail.com>
2020-11-23 23:41:05 +0530

    react assign

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,7 @@ function updateStructure(rec1,rec2){
 		const relativeDim=relative(rec1,rec2);
 		return{...rec1,children:[relativeDim]};
 	}else if(contains(rec1,rec2)){
+	    const relativeDim=relative(rec2,rec1);
 		return{...rec2,children:[relativeDim]};
 	}else{
 		return{...rec1};
@@ -33,22 +34,22 @@ function relative(rec1,rec2){
 		children:rec2.children
 	}
 	if(rec2.top){
-		res.top=`${recBn.x1-recAn.x1}px`;
+		res.top=`${rec2.x1-rec1.x1}px`;
 	}
-	if(rec.left){
-		res.left=`${recBn.y1-recAn.y1}px`;
+	if(rec2.left){
+		res.left=`${rec2.y1-rec1.y1}px`;
 	}
-	if(recBn.height){
-		res.height=recB.height;
+	if(rec2.height){
+		res.height=rec2.height;
 	}
-	if(recBn.width){
-		res.width=recB.width;
+	if(rec2.width){
+		res.width=rec2.width;
 	}
-	if(rec.bottom){
-		res.bottom=`${recAn.x2-recBn.x2}px`;
+	if(rec2.bottom){
+		res.bottom=`${rec1.x2-rec2.x2}px`;
 	}
-	if(rec.right){
-		res.right=`${recAn.y2-recBn.y2}px`;
+	if(rec2.right){
+		res.right=`${rec1.y2-rec2.y2}px`;
 	}
 	return res;
 }
@@ -57,7 +58,7 @@ const W=0;
 function normalize(rec){
 	return{
 		x1:rec.top?parseInt(rec.top):(T-(parseInt(rec.bottom)+parseInt(rec.height))),
-		y1:rec.left?parseInt(rec.left):(T-(parseInt(rec.right)+parseInt(rec.width))),
+		y1:rec.left?parseInt(rec.left):(W-(parseInt(rec.right)+parseInt(rec.width))),
 		x2:rec.bottom?(T-parseInt(rec.bottom)):(parseInt(rec.top)+parseInt(rec.height)),
 		y2:rec.right?(W-parseInt(rec.right)):(parseInt(rec.left)+parseInt(rec.width))
 	}
